ca86d615f0c4ea779b297141403deb5c55a77e9b
devinrader <devin@twilio.com>
2012-03-22 14:44:38 -0400

    Added call center controller

---
+++ MvcMusicStore/Controllers/CallCenterController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Twilio.TwiML.Mvc;
+using Twilio.TwiML;
+
+namespace MvcMusicStore.Controllers
+{
+    public class CallCenterController : TwilioController
+    {
+        //
+        // GET: /CallCenter/
+
+        public ActionResult Index()
+        {
+            var response = new TwilioResponse();
+            response.Pause(5);
+            response.Say("Thanks for calling the Music Store");            
+            return TwiML(response);
+        }
+    }
+}
